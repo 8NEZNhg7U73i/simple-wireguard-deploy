7bb3f4543ffe57bd73f933922d680846652027fc
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2021-09-20 03:01:04 -0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 for i in $(sudo -E apt list 2>/dev/null | grep -e "installed" | awk -F '/' '{print $1}'); do
     sudo -E echo $i >>remove.txt
+    sudo -E echo $i
     sudo -E apt-get remove $i 2>>/remove.txt 1>>remove.txt
 done
