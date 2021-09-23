09e4f5b85b7a7bfc3f2043cae5cb4d1c1be3e8ee
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2021-09-23 06:48:55 -0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,12 +1,36 @@
 #!/bin/bash -e
-show() {
+list() {
     for i in $(apt list 2>>/dev/stdout | grep -e "installed" | awk -F '/' '{print $1}'); do
         echo $i 2>>/dev/stdout >>/root/out/list.txt
+    done
+}
+show() {
+    for i in $(cat /root/out/list.txt); do
         apt-cache show $i 2>>/dev/stdout >>/root/out/show/$i.txt
+    done
+}
+showpkg() {
+    for i in $(cat /root/out/list.txt); do
         apt-cache showpkg $i 2>>/dev/stdout >>/root/out/showpkg/$i.txt
+    done
+}
+rdepends() {
+    for i in $(cat /root/out/list.txt); do
         apt-cache rdepends $i 2>>/dev/stdout >>/root/out/rdepends/$i.txt
+    done
+}
+depends() {
+    for i in $(cat /root/out/list.txt); do
         apt-cache depends $i 2>>/dev/stdout >>/root/out/depends/$i.txt
+    done
+}
+list_mod() {
+    for i in $(cat /root/out/list.txt); do
         echo $i,$(echo $i | grep -o "-" | grep -c "-") 2>>/dev/stdout >>/root/out/list_mod.txt
+    done
+}
+essential() {
+    for i in $(cat /root/out/list.txt); do
         (
             IFS=$'\n'
             for j in $(cat /root/out/show/$i.txt); do
@@ -32,4 +56,7 @@ mkdir /root/out/showpkg
 mkdir /root/out/rdepends
 mkdir /root/out/depends
 du -x / 2>>/dev/null 1>>/root/out/du.txt
-show
+list
+for i in show showpkg depends rdepends list_mod essential; do
+    $i &
+done
