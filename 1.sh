dde1772dc10261962c0f2f9cb7f44ac594625ebe
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2021-09-23 01:44:45 -0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,29 +1,32 @@
 #!/bin/bash -e
 show() {
-    for i in $(sudo -E apt list 2>/dev/stdout | grep -e "installed" | awk -F '/' '{print $1}'); do
-        sudo -E echo $i >>list.txt
-        sudo -E apt-cache show $i 2>>/dev/stdout >>./show/$i.txt
-        sudo -E apt-cache showpkg $i 2>>/dev/stdout >>./showpkg/$i.txt
-        sudo -E apt-cache rdepends $i 2>>/dev/stdout >>./rdepends/$i.txt
-        sudo -E apt-cache depends $i 2>>/dev/stdout >>./depends/$i.txt
-        echo $i,$(echo $i | grep -o "-" | grep -c "-") 2>>/dev/stdout >>list_mod.txt
+    for i in $(apt list 2>>/dev/stdout | grep -e "installed" | awk -F '/' '{print $1}'); do
+        echo $i 2>>/dev/stdout >>/root/out/list.txt
+        apt-cache show $i 2>>/dev/stdout >>/root/out/show/$i.txt
+        apt-cache showpkg $i 2>>/dev/stdout >>/root/out/showpkg/$i.txt
+        apt-cache rdepends $i 2>>/dev/stdout >>/root/out/rdepends/$i.txt
+        apt-cache depends $i 2>>/dev/stdout >>/root/out/depends/$i.txt
+        echo $i,$(echo $i | grep -o "-" | grep -c "-") 2>>/dev/stdout >>/root/out/list_mod.txt
         (
             IFS=$'\n'
             for j in $(cat ./show/$i.txt); do
-                #$temp="$j"
                 temp=$(echo $j | grep -x "Essential: yes")
-                #echo "$temp"
                 if [ "$temp" == "Essential: yes" ]; then
-                    echo "$i" 2>>/dev/stdout >>essential.txt
-                    echo "$i"
+                    echo "$i" 2>>/dev/stdout >>/root/out/essential.txt
+                    break
+                fi
+                if [ "$temp" == "Priority: required" ]; then
+                    echo "$i" 2>>/dev/stdout >>/root/out/essential.txt
                     break
                 fi
             done
         )
     done
 }
-mkdir show
-mkdir showpkg
-mkdir rdepends
-mkdir depends
+mkdir /root/out
+mkdir /root/out/show
+mkdir /root/out/showpkg
+mkdir /root/out/rdepends
+mkdir /root/out/depends
+du -x / 2>>/dev/null 1>>/root/out/du.txt
 show
