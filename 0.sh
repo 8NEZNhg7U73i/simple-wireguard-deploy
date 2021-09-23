782984ae654de59a6e469e83ef4ef9a3c1392799
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2021-09-23 00:49:23 -0700

    0

--- 0.sh
+++ 0.sh
@@ -1,2 +1,39 @@
 #!/bin/bash -e
 sudo -E du -x / 2>>/dev/null 1>>du.txt
+
+for i in $(ls); do
+    cat $i | while read -r; do
+        temp="$REPLY"
+        echo "$temp"
+        if [ "$temp" == "Package: "$temp"" ]; then
+            pkg="$REPLY"
+        fi
+        list=$(echo "$temp" | grep -x "Essential: yes")
+        if [ "$list" == "Essential: yes" ]; then
+            echo "$temp"
+        fi
+    done
+done
+
+show() {
+    for i in $(cat list.txt); do
+        sudo -E apt-cache show $i 2>>/dev/null >>./show/$i.txt
+        sudo -E apt-cache showpkg $i 2>>/dev/null >>./showpkg/$i.txt
+        sudo -E apt-cache rdepends $i 2>>/dev/null >>./rdepends/$i.txt
+        sudo -E apt-cache depends $i 2>>/dev/null >>./depends/$i.txt
+        echo $i,$(echo $i | grep -o "-" | grep -c "-") 2>>/dev/null >>list_mod.txt
+        (
+            IFS=$'\n'
+            for j in $(cat ./show/$i.txt); do
+                #$temp="$j"
+                temp=$(echo $j | grep -x "Essential: yes")
+                #echo "$temp"
+                if [ "$temp" == "Essential: yes" ]; then
+                    echo "$i" 2>>/dev/null >>essential.txt
+                    echo "$i"
+                    break
+                fi
+            done
+        )
+    done
+}
--- 1.sh
+++ 1.sh
@@ -1,2 +1,24 @@
 #!/bin/bash -e
-for i in $(sudo -E apt list 2>/dev/null | grep -e "installed" | awk -F '/' '{print $1}'); do sudo -E echo $i >>list.txt; done
+show() {
+    for i in $(cat list.txt); do
+        sudo -E apt-cache show $i 2>>/dev/null >>./show/$i.txt
+        sudo -E apt-cache showpkg $i 2>>/dev/null >>./showpkg/$i.txt
+        sudo -E apt-cache rdepends $i 2>>/dev/null >>./rdepends/$i.txt
+        sudo -E apt-cache depends $i 2>>/dev/null >>./depends/$i.txt
+        echo $i,$(echo $i | grep -o "-" | grep -c "-") 2>>/dev/null >>list_mod.txt
+        (
+            IFS=$'\n'
+            for j in $(cat ./show/$i.txt); do
+                #$temp="$j"
+                temp=$(echo $j | grep -x "Essential: yes")
+                #echo "$temp"
+                if [ "$temp" == "Essential: yes" ]; then
+                    echo "$i" 2>>/dev/null >>essential.txt
+                    echo "$i"
+                    break
+                fi
+            done
+        )
+    done
+}
+show
